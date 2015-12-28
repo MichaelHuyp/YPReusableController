b41b0514e3a45d485585d97ab1fbf2cad54bbf3e
MichaelHuyp <86812684@qq.com>
2015-12-28 17:03:45 +0800

    init
    init

---
+++ YPReusableController/YPReusableController/Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YPReusableController
+//
+//  Created by MichaelPPP on 15/12/28.
+//  Copyright (c) 2015年 tyiti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
